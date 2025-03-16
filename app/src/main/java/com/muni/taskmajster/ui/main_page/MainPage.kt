aa9affa33079e408668bcc6f7a0f95f54d918d6d
Kateřina Čepelková <523965@mail.muni.cz>
2025-03-16 15:15:47 +0100

    Base menu page
    TODO formating

--- app/src/main/java/com/muni/taskmajster/ui/main_page/MainPage.kt
+++ app/src/main/java/com/muni/taskmajster/ui/main_page/MainPage.kt
@@ -5,71 +5,60 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun MainPage () {
-        var presses by remember { mutableIntStateOf(0) }
-
         Scaffold(
             topBar = {
                 SpaceToolbar(
                     title = "Main Page"
                 )
             },
-            bottomBar = {
-                BottomAppBar(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    contentColor = MaterialTheme.colorScheme.primary,
-                ) {
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        textAlign = TextAlign.Center,
-                        text = "Bottom app bar",
-                    )
-                }
-            },
-            floatingActionButton = {
-                FloatingActionButton(onClick = { presses++ }) {
-                    Icon(Icons.Default.Add, contentDescription = "Add")
-                }
-            }
         ) { innerPadding ->
             Column(
                 modifier = Modifier
                     .padding(innerPadding),
                 verticalArrangement = Arrangement.spacedBy(16.dp),
             ) {
-                Text(
-                    modifier = Modifier.padding(8.dp),
-                    text =
-                    """
-                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
-
-                    It also contains some basic inner content, such as this text.
-
-                    You have pressed the floating action button $presses times.
-                """.trimIndent(),
+                MenuButton(
+                    text = "Play random",
+                    icon = Icons.Default.Refresh,
+                    onClick = { }
+                )
+                MenuButton(
+                    text = "Play gameplan",
+                    icon = Icons.Default.PlayArrow,
+                    onClick = { }
+                )
+                MenuButton(
+                    text = "Tasks",
+                    icon = Icons.Default.Menu,
+                    onClick = { }
+                )
+                MenuButton(
+                    text = "Tasks",
+                    icon = Icons.AutoMirrored.Filled.List,
+                    onClick = { }
                 )
             }
         }
@@ -79,32 +68,64 @@ fun MainPage () {
 fun SpaceToolbar(
     title: String,
 ) {
-    Column {
-        Row {
-            IconButton(
-                onClick = {},
-                content = {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Default.ArrowBack,
-                        contentDescription = null
-                    )
-                }
-            )
-            Spacer(modifier = Modifier.weight(1f))
-        }
+    Row {
+        IconButton(
+            onClick = {},
+            content = {
+                //Icon(
+                //    imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                //    contentDescription = null
+                //)
+            }
+        )
         Text(
             text = title,
             style = MaterialTheme.typography.titleLarge,
             modifier = Modifier
                 .padding(
                     top = 16.dp,
-                    start = 16.dp,
-                    end = 16.dp,
-                    bottom = 4.dp
+                    bottom = 16.dp,
                 )
         )
     }
 }
+
+@Composable
+fun MenuButton(
+    text: String,
+    icon: ImageVector,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(16.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Black
+        ),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(modifier = Modifier.padding(4.dp))
+            Text(
+                text = text,
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
+
 
 @Preview
 @Composable
