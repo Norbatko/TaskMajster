2ba7e1991eedc9be3133312da3df6ac689ed8b84
Tomáš Ebert <xebert@fi.muni.cz>
2025-03-23 20:06:46 +0100

    feat: Add permissions

--- app/src/main/java/com/muni/taskmajster/MainActivity.kt
+++ app/src/main/java/com/muni/taskmajster/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.muni.taskmajster
 
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.muni.taskmajster.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val permissions = if (Build.VERSION.SDK_INT >= 33) {
+            arrayOf(
+                android.Manifest.permission.READ_MEDIA_IMAGES
+
+            )
+        } else {
+            arrayOf(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        }
+
+        ActivityCompat.requestPermissions(
+            this,
+            permissions,
+            0
+        )
     }
+
+
 }
