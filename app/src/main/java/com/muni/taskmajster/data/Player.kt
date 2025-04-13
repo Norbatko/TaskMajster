d3b4e507fe8108eaea448ec14b712f88a7a6fa39
silviabielikova <sisa@bielik.sk>
2025-04-13 15:22:41 +0200

    #21 - feat: add createNew function in Player for empty players generation

--- app/src/main/java/com/muni/taskmajster/data/Player.kt
+++ app/src/main/java/com/muni/taskmajster/data/Player.kt
@@ -2,6 +2,7 @@ package com.muni.taskmajster.data
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kotlin.random.Random
 
 @Parcelize
 data class Player(
@@ -10,4 +11,16 @@ data class Player(
     val colour: Int, // todo may be changed in future acording to colour picker
     var totalPoints: Int,
     var taskPoints: Int,
-): Parcelable
+): Parcelable {
+    companion object {
+        fun createNew(): Player {
+            return Player(
+                id = System.currentTimeMillis(),
+                name = "Player X",
+                colour = Random.nextInt(),
+                totalPoints = 0,
+                taskPoints = 0
+            )
+        }
+    }
+}
