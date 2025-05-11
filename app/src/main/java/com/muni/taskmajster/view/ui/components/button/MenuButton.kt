910dfe7a679575a4897820a8f8537517ec5a392d
silviabielikova <sisa@bielik.sk>
2025-05-11 04:21:16 +0200

    #28 - feat: design buttons

--- app/src/main/java/com/muni/taskmajster/view/ui/components/button/MenuButton.kt
+++ app/src/main/java/com/muni/taskmajster/view/ui/components/button/MenuButton.kt
@@ -1,4 +1,4 @@
-package com.muni.taskmajster.view.ui.main_page
+package com.muni.taskmajster.view.ui.components.button
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -9,13 +9,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,12 +41,10 @@ fun MenuButton(
             Icon(
                 imageVector = icon,
                 contentDescription = null,
-                tint = Color.White
             )
             Spacer(modifier = Modifier.padding(4.dp))
             Text(
                 text = text,
-                color = Color.White,
                 style = MaterialTheme.typography.titleMedium
             )
         }
